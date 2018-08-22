e8f3c1d186b57ebc6289013e0dbae92333dbcc2a
Verf <verf@protonmail.com>
2018-08-22 16:48:14 +0800

    sys start

--- vulminer/entry.py
+++ vulminer/entry.py
@@ -3,45 +3,41 @@
 
 import click
 import utils
-import configparser
 import numpy as np
 from logger import logger
 from data import Data
-from training import Trainer
+#from training import Trainer
 
 """
 Entry Point, using click module to create command line app
 """
 
-BANNER = [" _   _ _____ _____   _____      _____  _    ",
-        "| \ | |_   _|  __ \ / ____|    |  __ \| |     ",
-        "|  \| | | | | |__) | |   ______| |  | | |     ",
-        "| . ` | | | |  ___/| |  |______| |  | | |     ",
-        "| |\  |_| |_| |    | |____     | |__| | |____ ",
-        "|_| \_|_____|_|     \_____|    |_____/|______|"]
-
+BANNER = """
+         _   _ _____ _____   _____      _____  _
+        | \ | |_   _|  __ \ / ____|    |  __ \| |
+        |  \| | | | | |__) | |   ______| |  | | |
+        | . ` | | | |  ___/| |  |______| |  | | |
+        | |\  |_| |_| |    | |____     | |__| | |____
+        |_| \_|_____|_|     \_____|    |_____/|______|
+"""
 
 @click.command()
 @click.option('-c', help = 'config file path')
 @click.option('-l', default = '.log', help = 'log file path')
 def main(c, l):
-    for i in BANNER:
-        print(i)
-
-    config = configparser.ConfigParser()
+    print(BANNER)
     if c:
-        config.read(c)
+        config = utils.yaml_load(c)
     else:
         logger.error('no config found')
 
     dataset = Data()
-    dataset.load(config['Input']['data_path'], config['Input']['data_type'])
+    dataset.load(config['Data'])
 
-    trainer = Trainer(config)
-    trainer.load_data(dataset.x_set, dataset.y_set)
-    trainer.training()
-    trainer.model.save('vulmodel.h5')
-
+    #trainer = Trainer()
+    #trainer.init(config)
+    #trainer.load(dataset.x_set, dataset.y_set)
+    #trainer.fit()
 
 if __name__ == '__main__':
     main()
