7179cf7750895dd1c6be1d5929721d729159253f
Verf <verf@protonmail.com>
2018-12-27 10:00:42 +0800

    developing

--- main-example.py
+++ main-example.py
@@ -7,73 +7,65 @@ Main entry of VulMiner
 :License: MIT
 """
 import os
+import pathlib
+import torch
 import torch.nn as nn
 from torch.optim import Adam
 from ignite.engine import Events
 from multiprocessing import cpu_count
-from covec.datasets import SySeVR
-from covec.processor import TextModel, Word2Vec
+from covec.datasets import SySeVR, Juliet
+from covec.processor import TextModel, TreeModel, Word2Vec
 from ignite.metrics import Precision, Recall
 from vulminer.utils import logger
 from vulminer.trainer import Trainer
-from vulminer.models.recurrent import GRU, BGRU
+from vulminer.models.recursive import CSTLTNN, NTLTNN, CBTNN
+from torch.utils.data import DataLoader
+
+dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # set work path
-data_root = os.path.expanduser('~/WorkSpace/Data')
-vulm_root = os.path.expanduser('~/WorkSpace/Test/vulminer/')
+data_root = pathlib.Path('~/WorkSpace/Test/Vul/Data').expanduser()
+vulm_root = pathlib.Path('~/WorkSpace/Test/Vul/').expanduser()
 
 # nn parameter
-input_size = 50
-hidden_size = 50
-num_layers = 2
+input_size = 100
+hidden_size = 200
 num_classes = 2
-dropout = 0.2
-learning_rate = 0.1
+learning_rate = 0.01
 
 # word2vec parameter
 word2vec_para = {
-    'size': input_size,
+    'size': int(input_size / 2),
     'min_count': 1,
     'workers': cpu_count(),
 }
 
-# dataloader parameter
-dataloader_para = {
-    'batch_size': 50,
-}
-
 # set neural network
-gru = GRU(
-    input_size,
-    hidden_size,
-    num_layers,
-    num_classes,
-    dropout=dropout,
-    batch_first=True,
-)
-bgru = BGRU(
-    input_size,
-    hidden_size,
-    num_layers,
-    num_classes,
-    dropout=dropout,
-    batch_first=True,
-)
+cstlstm = CSTLTNN(input_size, hidden_size, num_classes)
+ntlstm = NTLTNN(input_size, hidden_size, num_classes)
+cbtnn = CBTNN(input_size, num_classes)
 
 # set models
 models = [
     {
-        'nn': gru,
-        'optimizer': Adam(gru.parameters(), lr=learning_rate),
-        'loss': nn.CrossEntropyLoss(),
+        'nn': cstlstm.to(dev),
+        'optimizer': Adam(cstlstm.parameters(), lr=learning_rate),
+        'loss': nn.BCEWithLogitsLoss(),
+        'batch_size': 50,
         'epoch': 5,
     },
-    {
-        'nn': bgru,
-        'optimizer': Adam(bgru.parameters(), lr=learning_rate),
-        'loss': nn.CrossEntropyLoss(),
-        'epoch': 5,
-    },
+    # {
+    #     'nn': ntlstm.to(dev),
+    #     'optimizer': Adam(cstlstm.parameters(), lr=learning_rate),
+    #     'loss': nn.BCEWithLogitsLoss(),
+    #     'epoch': 5,
+    # },
+    # {
+    #     'nn': cbtnn.to(dev),
+    #     'optimizer': Adam(cstlstm.parameters(), lr=learning_rate),
+    #     'loss': nn.BCEWithLogitsLoss(),
+    #     'epoch': 5,
+    # },
 ]
 
 # set metrics
@@ -95,31 +87,27 @@ def log_iter_10(trainer, evaluator, train, valid):
 def main():
     # logger config
     logger.level = 'debug'
-    logger.addFileHandler(path=vulm_root + 'vulminer.log')  # set log file path
+    logger.addFileHandler(path=str(vulm_root / 'vulminer.log'))
     # dataset config
     # set word2vec, the parameter same as gensim's word2vec
     embedder = Word2Vec(**word2vec_para)
     # set processor, Text Model need a embedder
-    processor = TextModel(embedder)
-    # set dataset
-    dataset = SySeVR(data_root)
+    processor = TreeModel(embedder, 100)
+    # set dataset ['AE', 'AF', 'AU', 'PU']
+    dataset = Juliet(
+        str(data_root),
+        processor,
+        proxy='socks5://127.0.0.1:1080',
+        category=['AF'])
     # use processor to process dataset, you can call use
-    dataset.process(processor, category=['AF'])
     # get pytorch dataset object
-    torchset = dataset.torchset()
     # trainer config
-    trainer = Trainer(vulm_root)
+    trainer = Trainer(str(vulm_root))
     # add dataset, the trainer can only have one dataset
     # the old dataset will replace by the new dataset when called
     # optional, you can input parameter for pytorch dataloader
-    trainer.addData(torchset, **dataloader_para)
-    # add metrics
-    trainer.addMetrics(metrics)
-    # add models
+    trainer.addData(dataset)
     trainer.addModel(models)
-    # add extra event handler for trainer
-    trainer.addEvent(Events.ITERATION_COMPLETED, log_iter_10)
-    # start training, you can input a int number to start a n-folds validation
     trainer.fit(5)
 
 
