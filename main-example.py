dc27cedb00f79a78310459436f0455fb62758f88
Verf <verf@protonmail.com>
2019-02-18 10:35:59 +0800

    developing

--- main-example.py
+++ main-example.py
@@ -14,7 +14,7 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 from multiprocessing import cpu_count
 from covec.datasets import SySeVR, Juliet, VulDeePecker
-from covec.processor import TextModel, TreeModel, Word2Vec
+from covec.processor import TextModel, TreeModel, Word2Vec, Tree2Seq
 from vulminer.utils import logger
 from vulminer.trainer import Trainer
 from vulminer.models.recurrent import GRU, BGRU
@@ -28,7 +28,7 @@ data_root = pathlib.Path('~/Workspace/Merge/Test/Data').expanduser()
 vulm_root = pathlib.Path('~/Workspace/Merge/Test/').expanduser()
 
 # nn parameter
-input_size = 100
+input_size = 50
 hidden_size = 200
 num_layers = 2
 num_classes = 2
@@ -36,22 +36,19 @@ learning_rate = 0.001
 
 # word2vec parameter
 word2vec_para = {
-    'size': int(input_size),
+    'size': input_size,
     'min_count': 1,
     'workers': cpu_count(),
 }
 
 # set neural network
-cstlstm = CSTLTNN(input_size, hidden_size, num_classes)
-ntlstm = NTLTNN(input_size, hidden_size, num_classes)
-cbtnn = CBTNN(input_size, num_classes)
-gru = GRU(input_size, hidden_size, num_layers, num_classes)
-bgru = BGRU(input_size, hidden_size, num_layers, num_classes)
+gru = GRU(input_size*2, hidden_size, num_layers, num_classes)
+bgru = BGRU(input_size*2, hidden_size, num_layers, num_classes)
 
 # set models
 models = [
     {
-        'nn': gru,
+        'nn': bgru,
         'opti': Adam(gru.parameters(), lr=learning_rate),
         'loss': nn.CrossEntropyLoss(),
         'batch_size': 100,
@@ -96,16 +93,21 @@ def main():
     # set word2vec, the parameter same as gensim's word2vec
     embedder = Word2Vec(**word2vec_para)
     # set processor, Text Model need a embedder
-    processor = TextModel(embedder)
+    processor = Tree2Seq(embedder, 100)
     # set dataset
-    dataset = SySeVR(str(data_root), processor)
+    dataset = Juliet(str(data_root), processor)
     # set trainer
     trainer = Trainer(str(vulm_root))
     # add data and models
     trainer.addData(dataset)
     trainer.addModel(models)
     trainer.addMetrics(metrics)
-    trainer.fit(['AF'], 10)
+    trainer.fit([
+        'CWE121',
+        'CWE122',
+        'CWE123',
+        'CWE124',
+        ], 10)
 
 
 if __name__ == '__main__':
